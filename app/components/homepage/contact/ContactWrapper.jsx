0a67ce0431a68e27eda0da1e675fa951b235fc09
seeni <seenivasan9427@gmail.com>
2025-10-08 20:00:40 +0530

    Fixed contact form SSR error

--- app/components/homepage/contact/ContactWrapper.jsx
+++ app/components/homepage/contact/ContactWrapper.jsx
@@ -1,9 +1,5 @@
 "use client";
-
-import dynamic from "next/dynamic";
-
-// Dynamically import the contact form so it only renders on the client
-const ContactForm = dynamic(() => import("./contact-form"), { ssr: false });
+import ContactForm from "./contact-form";
 
 export default function ContactWrapper() {
   return <ContactForm />;
